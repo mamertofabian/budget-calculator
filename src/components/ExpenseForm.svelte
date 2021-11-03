fa090ee6785d0146cc49d0ef5719c2644ef5e9be
Mamerto Fabian <mfabianjr@gmail.com>
2021-11-03 16:28:06 +0800

    Added empty handleSubmit with preventDefault modifier

--- src/components/ExpenseForm.svelte
+++ src/components/ExpenseForm.svelte
@@ -5,11 +5,17 @@
   let amount = null;
   // $: console.log({name, amount});
   $: isEmpty = !name || !amount;
+
+  const handleSubmit = () => {
+    console.log({name, amount});
+    name = '';
+    amount = null;
+  };
 </script>
 
 <section class="form">
   <Title title="add expense"/>
-  <form class="expense-form">
+  <form class="expense-form" on:submit|preventDefault={handleSubmit}>
     <div class="form-control">
       <label for="name">name</label>
       <input type="text" id="name" bind:value={name}>
