506fcb0580775568365c0a0420fc6ec71f75d951
Mamerto Fabian <mfabianjr@gmail.com>
2021-11-03 20:39:10 +0800

    Proper each block transition and flip animation

--- src/components/ExpenseList.svelte
+++ src/components/ExpenseList.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-  import Expense from "./Expense.svelte";
-  import type { ExpenseInterface } from "../modules/expenses";
-  import SectionTitle from "./Title.svelte";
+  import { fly } from 'svelte/transition';
+  import { flip } from 'svelte/animate'
+  import Expense from './Expense.svelte';
+  import type { ExpenseInterface } from '../modules/expenses';
+  import SectionTitle from './Title.svelte';
 
   export let expenses: ExpenseInterface[] = [];
 </script>
 
 <section>
-  <SectionTitle title="expense list" />
+  <SectionTitle title="expense list"/>
   <ul>
-    {#each expenses as expense, index}
-      <Expense {...expense} />
+    {#each expenses as expense, index (expense.id)}
+      <div in:fly={{x:200, delay:(index + 1) * 200}} out:fly={{x:-200}} animate:flip>
+        <Expense {...expense}/>
+      </div>
     {:else}
       <h2>No expenses added to the list</h2>
     {/each}
@@ -18,7 +22,7 @@
 </section>
 
 <style>
-  h2 {
-    text-transform: capitalize;
-  }
+    h2 {
+        text-transform: capitalize;
+    }
 </style>
